01379e5b63b6d43bf73a82150b4ac521df291729
Benjamin Neff & Felix Becker <benjamin@coding4coffee.ch>
2011-03-06 00:34:16 +0100

    parameter fuer serve#r

--- src/main/scala/de/tdng2011/game/visualizer/Main.scala
+++ src/main/scala/de/tdng2011/game/visualizer/Main.scala
@@ -5,25 +5,29 @@ import swing._
 
 object Main extends SwingApplication{
   def startup(args: Array[String]) {
-    val dialog = new MainFrame {
-      title = "Connect"
-      val inputField = new TextField {
-        text = "localhost"
-        columns = 20
-        selectAll
-      }
-      val connectionButton = new Button("Connect!") {
-        reactions += {
-          case x:ButtonClicked => {
-            new Client(inputField.text)
-            close
+    if (args.size > 0) {
+      new Client(args(0))
+    } else {
+      val dialog = new MainFrame {
+        title = "Connect"
+        val inputField = new TextField {
+          text = "localhost"
+          columns = 20
+          selectAll
+        }
+        val connectionButton = new Button("Connect!") {
+          reactions += {
+            case x:ButtonClicked => {
+              new Client(inputField.text)
+              close
+            }
           }
         }
+        defaultButton = connectionButton
+        contents = new FlowPanel(inputField, connectionButton)
+        peer.setLocationRelativeTo(null)
+        visible = true
       }
-      defaultButton = connectionButton
-      contents = new FlowPanel(inputField, connectionButton)
-      peer.setLocationRelativeTo(null)
-      visible = true
     }
   }
 }
