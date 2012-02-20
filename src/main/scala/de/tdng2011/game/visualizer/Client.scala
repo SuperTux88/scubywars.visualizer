4737bf2ddec1a6296415d99ea392730de0461029
Felix Becker <git@felixbecker.name>
2012-02-20 14:48:52 +0100

    zwischenstand

--- src/main/scala/de/tdng2011/game/visualizer/Client.scala
+++ src/main/scala/de/tdng2011/game/visualizer/Client.scala
@@ -7,8 +7,10 @@ class Client(hostname: String, val sound: Boolean = false) extends AbstractClien
 
   Visualizer.start
 
-  if (sound) new SoundPlayer("/background.wav", true)
-
+  if (sound){
+   new SoundPlayer("/background.wav", true).playSound()
+  }
+  
   def processWorld(world: World) {
     Visualizer !! world
   }
@@ -36,10 +38,20 @@ class Client(hostname: String, val sound: Boolean = false) extends AbstractClien
       first.playSound
     }
   }
+  
+  override def playerCollisionEvent(){
+    if(sound){
+    	val x = scala.util.Random.shuffle(Sounds.playerCollisionSounds)
+    	var first = x(0)
+    	first.playSound
+    }
+  }
+  
 }
 
 object Sounds {
-  val shotSounds = 0 to 4 map (i => new SoundPlayer("/shotSpawned" + i + ".wav"))
-  val killSounds = 0 to 5 map (i => new SoundPlayer("/playerKilled" + i + ".wav"))
+  val shotSounds = 0 to 4 map (i => new SoundPlayer("/shotSpawned" + i + ".wav", false, -5))
+  val killSounds = 0 to 5 map (i => new SoundPlayer("/playerKilled" + i + ".wav", false, -5))
+  val playerCollisionSounds = 0 to 1 map (i => new SoundPlayer("/playerCollision"+i+".wav"))
 }
 
--- src/main/scala/de/tdng2011/game/visualizer/SoundPlayer.scala
+++ src/main/scala/de/tdng2011/game/visualizer/SoundPlayer.scala
@@ -3,6 +3,7 @@ package de.tdng2011.game.visualizer
 import actors.Actor
 import javax.sound.sampled.{ LineEvent, AudioSystem }
 import javax.sound.sampled.Clip
+import javax.sound.sampled.FloatControl
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,13 +12,16 @@ import javax.sound.sampled.Clip
  * Time: 16:01
  */
 
-class SoundPlayer(fileName: String, repeat: Boolean = false) {
+class SoundPlayer(fileName: String, repeat: Boolean = false, gain : Float = 0) {
   val now = System.currentTimeMillis()
   val file = getClass getResource fileName
   val stream = AudioSystem getAudioInputStream file
   val clip = AudioSystem.getClip()
   clip.open(stream)
-
+ 
+  val volume = clip.getControl(FloatControl.Type.MASTER_GAIN).asInstanceOf[FloatControl]
+  volume.setValue(gain)
+  
   if (repeat) {
     clip.setLoopPoints(0, -1)
     clip.loop(Clip.LOOP_CONTINUOUSLY)
