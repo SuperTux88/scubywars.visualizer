68c998defa7d584cd443809bfbbe11798a8f3e0f
Benjamin Neff <benjamin@coding4coffee.ch>
2012-02-20 16:44:26 +0100

    events

--- src/main/scala/de/tdng2011/game/visualizer/Client.scala
+++ src/main/scala/de/tdng2011/game/visualizer/Client.scala
@@ -1,16 +1,17 @@
 package de.tdng2011.game.visualizer
 
 import de.tdng2011.game.library.connection.{ RelationTypes, AbstractClient }
+import de.tdng2011.game.library.event.{ CollisionEvent, PlayerKilledEvent, ShotSpawnedEvent }
 import de.tdng2011.game.library.World
 
 class Client(hostname: String, val sound: Boolean = false) extends AbstractClient(hostname, RelationTypes.VisualizerNG) {
 
   Visualizer.start
 
-  if (sound){
-   new SoundPlayer("/background.wav", true)
+  if (sound) {
+    new SoundPlayer("/background.wav", true)
   }
-  
+
   def processWorld(world: World) {
     Visualizer !! world
   }
@@ -23,7 +24,7 @@ class Client(hostname: String, val sound: Boolean = false) extends AbstractClien
     Visualizer.currentNames = names
   }
 
-  override def shotSpawnedEvent() {
+  override def shotSpawnedEvent(event: ShotSpawnedEvent) {
     if (sound) {
       val x = scala.util.Random.shuffle(Sounds.shotSounds)
       var first = x(0)
@@ -31,27 +32,27 @@ class Client(hostname: String, val sound: Boolean = false) extends AbstractClien
     }
   }
 
-  override def playerKilledEvent() {
+  override def playerKilledEvent(event: PlayerKilledEvent) {
     if (sound) {
       val x = scala.util.Random.shuffle(Sounds.killSounds)
       var first = x(0)
       first.playSound
     }
   }
-  
-  override def playerCollisionEvent(){
-    if(sound){
-    	val x = scala.util.Random.shuffle(Sounds.playerCollisionSounds)
-    	var first = x(0)
-    	first.playSound
+
+  override def playerCollisionEvent(event: CollisionEvent) {
+    if (sound) {
+      val x = scala.util.Random.shuffle(Sounds.playerCollisionSounds)
+      var first = x(0)
+      first.playSound
     }
   }
-  
+
 }
 
 object Sounds {
   val shotSounds = 0 to 4 map (i => new SoundPlayer("/shotSpawned" + i + ".wav", false, -5))
   val killSounds = 0 to 5 map (i => new SoundPlayer("/playerKilled" + i + ".wav", false, -5))
-  val playerCollisionSounds = 0 to 1 map (i => new SoundPlayer("/playerCollision"+i+".wav"))
+  val playerCollisionSounds = 0 to 1 map (i => new SoundPlayer("/playerCollision" + i + ".wav"))
 }
 
